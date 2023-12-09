36b94927b5fea8aae182d0d3e85daef27bfb8887
NaslyRamos <naslyramos404@gmail.com>
2023-12-08 22:15:14 -0500

    Formulario de busqueda sencillo

---
+++ vistas/form_busqueda.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="../style/estilo_form_busqueda.css"> <!-- Enlace al archivo CSS -->
+    <title>Document</title>
+</head>
+<body>
+<div class="main-content">
+
+<form class="form-search" method="get" action="result_busqueda.php">
+    <h1>FORMULARIO DE BUSQUEDA</h1>
+    <input type="search" name="search"  placeholder="Buscar.." id="buscar">
+    <i class="fa fa-search"></i>
+    <button type="submit" >Enviar</button>
+    
+</form>
+</div>
+</body>
+</html>
